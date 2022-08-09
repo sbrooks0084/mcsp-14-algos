9ff4670410415f1c9ac98d163a360594f26f3fe3
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-08-09 10:39:20 -0500

    Added html file and instructions

---
+++ splitter.js
@@ -0,0 +1,20 @@
+// Write the splitter function that: 
+
+// Takes in an array and a num
+// returns an array of subarray of length num
+
+// notes:
+
+    // Subarrays cannot have a length of > num
+    // sub array CAN have a length that is <= num
+
+function splitter(arr, num) {
+    // your code here
+}
+
+const arr = [1,2,3,4,5,6,7,8,9]
+const num = 3
+
+// what 
+
+// console.log(splitter(arr, num))
